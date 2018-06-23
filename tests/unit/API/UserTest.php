31bf1ff4f3af9f8ab4ec4af095c8799a770da8e1
Lee Jones <lee@dragonflyrg.com>
2018-06-23 16:40:33 -0400

    Updates to unit tests. UserAPI now uses IWriteRepository.

--- tests/unit/API/UserTest.php
+++ tests/unit/API/UserTest.php
@@ -2,34 +2,46 @@
 
 namespace Tests\unit\API;
 
-use DDP\Domain\User\Infrastructure\UserRepository;
+use DDP\Core\Infrastructure\IWriteRepository;
 use DDP\Domain\User\Domain\UserWithRoles;
 use PHPUnit\Framework\TestCase;
 use DDP\API\User;
+
+
+class MockRepo implements IWriteRepository
+{
+	public function save( $Object )
+	{
+		if( $Object->getIdentifier() == 0 )
+			return false;
+
+		return true;
+	}
+}
 
 class UserTest extends TestCase
 {
 	public function testRegisterAdmin()
 	{
-		$Repo = $this
-			->getMockBuilder( UserRepository::class )
-			->setMethods(['save'])
-			->disableOriginalConstructor()
-			->getMock();
-
-		$UserApi = new User(
-			$Repo
-		);
-
-		$Repo->expects( $this->any() )
-			->method('save')
-			->will(
-				$this->returnValue( true )
-			);
-
 		$User = new UserWithRoles();
 
-		$this->assertTrue( $UserApi->registerAdmin( $User ) );
+		$User->setIdentifier( 0 );
+
+		$this->assertFalse(
+			User::registerAdmin(
+				new MockRepo(),
+				$User
+			)
+		);
+
+		$User->setIdentifier( 1 );
+
+		$this->assertTrue(
+			User::registerAdmin(
+				new MockRepo(),
+				$User
+			)
+		);
 
 	}
 }
