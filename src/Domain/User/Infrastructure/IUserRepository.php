e84d0ec0ca725c7f6439755104160439f87a65bb
Senik Hakobyan <senikhakobyan@Seniks-iMac.local>
2018-08-14 19:49:03 +0400

    interface fixed

--- src/Domain/User/Infrastructure/IUserRepository.php
+++ src/Domain/User/Infrastructure/IUserRepository.php
@@ -4,7 +4,6 @@ namespace DDP\Domain\User\Infrastructure;
 
 interface IUserRepository
 {
-	public function getUserModel();
 	public function getAll( array $Params ): array;
 	public function getById( $UserId );
 	public function save( $User );
