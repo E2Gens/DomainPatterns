4c12f1774a739e9502235a56148e23ffb7cdf325
Lee Jones <lee@dragonflyrg.com>
2018-08-20 11:17:45 -0400

    fix to application::init

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -3,12 +3,14 @@
 namespace DDP\Core;
 
 use DDP\Core\Infrastructure\DbLogDestination;
+use Neuron\Log\Destination\DestinationBase;
 use Neuron\Log\Format\PlainText;
 use Neuron\Log\Log;
+use Neuron\Log\Logger;
 
 class Application
 {
-	public static function init()
+	public static function init( DestinationBase $Destination )
 	{
 		$Log = Log::getInstance();
 
@@ -21,9 +23,9 @@ class Application
 		// Add database logger
 
 		$Log->Logger->addLog(
-			new DbLogDestination(
-				new PlainText(),
-				new \App\Log )
+			new Logger(
+				$Destination
+			)
 		);
 
 		// Stuff it back into the singleton
