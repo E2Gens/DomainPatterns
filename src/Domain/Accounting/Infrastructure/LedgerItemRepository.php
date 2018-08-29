a3b50ac4e9cfa9a6fc71495707d2e4a8a3a3e2f7
Lee Jones <lee@dragonflyrg.com>
2018-08-29 16:55:44 -0400

    Completed ledgerrepo getbyid.

--- src/Domain/Accounting/Infrastructure/LedgerItemRepository.php
+++ src/Domain/Accounting/Infrastructure/LedgerItemRepository.php
@@ -2,13 +2,27 @@
 
 namespace DDP\Domain\Accounting\Infrastructure;
 
+use DDP\Domain\Accounting\Domain\LedgerItem;
 use Neuron\Data\Object\DateRange;
 
 class LedgerItemRepository
 {
-	public function getById( int $LedgerItemId )
-	{
+	private $_LedgerModel;
 
+	public function __construct( \App\Ledger $Ledger )
+	{
+		$this->_LedgerModel = $Ledger;
+	}
+
+	public function getById( int $LedgerItemId ) : LedgerItem
+	{
+		$Data = $this->_LedgerModel->where( 'id', $LedgerItemId )->first()->toArray();
+
+		$Ledger = new LedgerItem();
+
+		LedgerItem::fromArray( $Ledger, $Data );
+
+		return $Ledger;
 	}
 
 	public function getByDateRange( DateRange $Range ) : array
@@ -19,6 +33,9 @@ class LedgerItemRepository
 		{
 			throw new \Exception( "Invalid date range: ".$Range->Start.'-'.$Range->End );
 		}
+
+		// @todo
+		throw new \Exception( 'not implemented' );
 	}
 
 	public function getByAccountAndDateRange( int $AccountId, DateRange $Range )
@@ -30,6 +47,7 @@ class LedgerItemRepository
 			throw new \Exception( "Invalid date range: ".$Range->Start.'-'.$Range->End );
 		}
 
-
+		// @todo
+		throw new \Exception( 'not implemented' );
 	}
 }
